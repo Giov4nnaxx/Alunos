38e528c3e53e7b9732344a1a0f1588885c864ea7
Giov4nnaxx <gigisantanasilva@gmail.com>
2025-09-17 08:24:40 -0300

    HTML

--- server.js
+++ server.js
@@ -1,40 +1,65 @@
 // importando express
 const express = require("express");
+const mysql = require("mysql2/promise");
 const app = express();
 app.use(express.json());
 const porta = 3000;
 
-const alunos = [{
-    id: 1,
-        nome: "Maria Silva",
-        cpf: "12345678901",
-        cep: "01234567",
-        uf: "SP",
-        rua: "Av. Central",
-        numero: 123,
-        complemento: "Apto 12"
-}
-]
+const conexao = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "Senai2025",
+    database: "escola_db",
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
 
-app.get("/alunos",(req,res) =>{
-    res.json(alunos)
+
+app.get("/alunos", async (req, res) => {
+    try {
+        const [retorno] = await conexao.query("SELECT * FROM alunos")
+        //console.log(retorno)
+        res.status(200).json(retorno)
+    } catch (erro) {
+        console.log(erro);
+        res.status(500).json({ erro: "Erro ao buscar alunos" })
+    }
+})
+
+app.post("/alunos", async (req, res) => {
+    try {
+        const { nome, cpf, cep = null, uf = null, rua = null, numero = null, complemento = null } = req.body;
+
+        if (!nome || !cpf) return res.status(400).json({ msg: "Nome e CPF são obrigatórios" })
+
+        const resultado = await conexao.query(
+            "INSERT INTO alunos (nome,cpf,cep,uf,rua,numero,complemento) VALUES (?,?,?,?,?,?,?)",
+            [nome, cpf, cep, uf, rua, numero, complemento]
+        );
+        res.status(201).json({ mensagem: "Aluno cadastrado com sucesso!" })
+    } catch (erro) {
+        console.log(erro)
+        res.status(500).json({ erro: "Erro ao inserir o aluno" })
+    }
 })
 // http://localhost:3000/alunos/2
-app.get("/alunos/:id",(req,res) =>{
+app.get("/alunos/:id", (req, res) => {
     const id = parseInt(req.params.id)
-    const aluno = alunos.find( (aluno => aluno.id === id))
+    const aluno = alunos.find((aluno => aluno.id === id))
 
-    if(aluno){
+    if (aluno) {
         res.json(aluno)
-    }else{
+    } else {
         res.status(404).json(
-            {erro: "Aluno não encontrado"}
+            { erro: "Aluno não encontrado" }
         )
     }
-   
+
 })
 
-function validarAluno({nome, cpf, cep, uf, rua, numero}) {
+function validarAluno({ nome, cpf, cep, uf, rua, numero }) {
     if (!nome) return "Nome é obrigatório";
     if (!cpf) return "CPF é obrigatório";
     if (!cep) return "Cep é obrigatório";
@@ -44,23 +69,23 @@ function validarAluno({nome, cpf, cep, uf, rua, numero}) {
     if (uf.length !== 2 || !String(uf)) return "UF é só 2 caracteres";
     if (!Number(numero)) return "Número Inválido";
     if (cpf.length !== 11 || !Number(cpf)) return "CPF Inválido";
-    if(cep.length !== 8 || !Number(cep)) return "Cep Inválido";
+    if (cep.length !== 8 || !Number(cep)) return "Cep Inválido";
     return null;
 }
 
 app.post("/alunos", (req, res) => {
-    const {nome,cpf,cep,uf,rua,numero,complemento} = req.body;
-    const erro = validarAluno({nome, cpf, cep, uf, rua, numero});
+    const { nome, cpf, cep, uf, rua, numero, complemento } = req.body;
+    const erro = validarAluno({ nome, cpf, cep, uf, rua, numero });
     if (erro) return res.status(400).json({ erro });
 
     const cpfRep = alunos.find(cpfRep => cpfRep.cpf === cpf)
-    if(cpfRep){
+    if (cpfRep) {
         return res.status(409).json({
             erro: "CPF já cadastrado"
         })
     }
     const id = alunos.length > 0 ? alunos[alunos.length - 1].id + 1 : 1
-    const novoAluno = {id,nome,cpf,cep,uf,rua,numero,complemento}
+    const novoAluno = { id, nome, cpf, cep, uf, rua, numero, complemento }
     alunos.push(novoAluno)
     res.status(201).json({
         mensagem: "Aluno Cadastrado com sucesso",
@@ -68,20 +93,20 @@ app.post("/alunos", (req, res) => {
     })
 })
 
-app.put("/alunos/:id", (req, res)=>{ 
+app.put("/alunos/:id", (req, res) => {
     const id = parseInt(req.params.id)
-    const {nome,cpf,cep,uf,rua,numero,complemento} = req.body;
+    const { nome, cpf, cep, uf, rua, numero, complemento } = req.body;
     const aluno = alunos.find(aluno => aluno.id === id)
-    if(!aluno){
+    if (!aluno) {
         return res.status(400).json({
             erro: "Aluno não encontrado"
         })
     }
-    const erro = validarAluno({nome, cpf, cep, uf, rua, numero});
+    const erro = validarAluno({ nome, cpf, cep, uf, rua, numero });
     if (erro) return res.status(400).json({ erro });
 
     const cpfRep = alunos.find(cpfRep => cpfRep.cpf === cpf && cpfRep.id !== id)
-    if(cpfRep){
+    if (cpfRep) {
         return res.status(409).json({
             erro: "CPF já cadastrado"
         })
@@ -98,17 +123,17 @@ app.put("/alunos/:id", (req, res)=>{
     })
 })
 
-app.delete("/alunos/:id",(req,res)=>{
+app.delete("/alunos/:id", (req, res) => {
     const id = parseInt(req.params.id);
 
     const indice = alunos.findIndex(a => a.id === id)
 
-    if(indice === -1){
+    if (indice === -1) {
         return res.status(404).json({
             mensagem: "Aluno não encontrado"
         })
     }
-    alunos.splice(indice,1);
+    alunos.splice(indice, 1);
 
     res.status(200).json({
         mensagem: "Aluno Deletado com sucesso"
@@ -116,4 +141,4 @@ app.delete("/alunos/:id",(req,res)=>{
 });
 
 
-app.listen(porta, () => console.log( `Servidor rodando http://localhost:${porta}/`));
+app.listen(porta, () => console.log(`Servidor rodando http://localhost:${porta}/`));
